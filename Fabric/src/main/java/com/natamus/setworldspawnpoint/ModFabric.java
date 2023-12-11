32181a84ce061645817aef7dcfe998f7dea20fb2
Rick South <rick@natamus.com>
2023-12-12 00:11:06 +0100

    Set World Spawn Point source code for 1.19.3.

---
+++ Fabric/src/main/java/com/natamus/setworldspawnpoint/ModFabric.java
@@ -0,0 +1,44 @@
+package com.natamus.setworldspawnpoint;
+
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.fabric.callbacks.CollectiveMinecraftServerEvents;
+import com.natamus.setworldspawnpoint.events.WorldSpawnEvent;
+import com.natamus.setworldspawnpoint.util.Reference;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.entity.event.v1.ServerPlayerEvents;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerEntityEvents;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.storage.ServerLevelData;
+
+public class ModFabric implements ModInitializer {
+	
+	@Override
+	public void onInitialize() {
+		setGlobalConstants();
+		ModCommon.init();
+
+		loadEvents();
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadEvents() {
+		CollectiveMinecraftServerEvents.WORLD_SET_SPAWN.register((ServerLevel serverLevel, ServerLevelData serverLevelData) -> {
+			WorldSpawnEvent.onWorldLoad(serverLevel, serverLevelData);
+		});
+
+		ServerPlayerEvents.AFTER_RESPAWN.register((ServerPlayer oldPlayer, ServerPlayer newPlayer, boolean alive) -> {
+			WorldSpawnEvent.onPlayerRespawn(oldPlayer, newPlayer, alive);
+		});
+
+		ServerEntityEvents.ENTITY_LOAD.register((Entity entity, ServerLevel world) -> {
+			WorldSpawnEvent.onEntityJoin(world, entity);
+		});
+	}
+
+	private static void setGlobalConstants() {
+
+	}
+}
