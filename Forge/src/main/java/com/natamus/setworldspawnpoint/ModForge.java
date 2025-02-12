6127c2497f94147b12939d0b47b10d546ae4cb96
Rick South <rick@natamus.com>
2025-02-12 23:14:59 +0100

    (v3.5) (Forge & Neo) Removed redundant event register annotations. [+1]

--- Forge/src/main/java/com/natamus/setworldspawnpoint/ModForge.java
+++ Forge/src/main/java/com/natamus/setworldspawnpoint/ModForge.java
@@ -32,7 +32,7 @@ public class ModForge {
 	}
 
 	private void loadComplete(final FMLLoadCompleteEvent event) {
-    	MinecraftForge.EVENT_BUS.register(new ForgeWorldSpawnEvent());
+    	MinecraftForge.EVENT_BUS.register(ForgeWorldSpawnEvent.class);
 	}
 
 	private static void setGlobalConstants() {
