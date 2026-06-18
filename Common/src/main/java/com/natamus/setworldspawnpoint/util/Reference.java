add24c9fbec931c63a2275d5173a798989ad00b0
Rick South <rick@natamus.com>
2026-06-18 22:27:37 +0200

    Updated Set World Spawn Point to Minecraft version 26.2.0.

--- Common/src/main/java/com/natamus/setworldspawnpoint/util/Reference.java
+++ Common/src/main/java/com/natamus/setworldspawnpoint/util/Reference.java
@@ -4,5 +4,5 @@ public class Reference {
 	public static final String MOD_ID = "setworldspawnpoint";
 	public static final String NAME = "Set World Spawn Point";
 	public static final String VERSION = "3.8";
-	public static final String ACCEPTED_VERSIONS = "[26.1.2]";
+	public static final String ACCEPTED_VERSIONS = "[26.2.0]";
 }
